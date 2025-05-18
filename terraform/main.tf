b0923690d52f9065ed25c8ad3c1df99822fdb484
kopistas <147347325+kopistas@users.noreply.github.com>
2025-05-18 13:29:25 +0200

    update backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,9 +18,9 @@ terraform {
   backend "s3" {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
-    endpoint                    = "https://${var.do_spaces_region}.digitaloceanspaces.com"
+    endpoint                    = "https://ams3.digitaloceanspaces.com"
     region                      = "us-east-1" # Required but unused for DO Spaces
-    bucket                      = "${var.do_spaces_name}"
+    bucket                      = "terraform-storage-moria"
     key                         = "terraform-monet.tfstate"
   }
 }
