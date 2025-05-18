09df6fd5318552f038d3c1e9aee959f48479db79
I <abstraktrnay.girl@gmail.com>
2025-05-18 21:24:22 +0200

    фикс переменных

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,67 +77,80 @@ module "kubernetes_resources" {
 variable "do_token" {
   description = "DigitalOcean API token"
   sensitive = true
+  type      = string
 }
 
 variable "do_region" {
   description = "DigitalOcean region"
   default = "ams3"
+  type    = string
 }
 
 variable "do_spaces_region" {
   description = "DigitalOcean Spaces region"
   default = "ams3"
+  type    = string
 }
 
 variable "do_spaces_name" {
   description = "DigitalOcean Spaces bucket name"
   default = "monet-models"
+  type    = string
 }
 
 variable "do_spaces_access_key" {
   description = "DigitalOcean Spaces access key"
   sensitive = true
+  type      = string
 }
 
 variable "do_spaces_secret_key" {
   description = "DigitalOcean Spaces secret key"
   sensitive = true
+  type      = string
 }
 
 variable "cluster_node_size" {
   description = "Size of the Kubernetes nodes"
   default = "s-2vcpu-4gb"
+  type    = string
 }
 
 variable "cluster_node_count" {
   description = "Number of nodes in the Kubernetes cluster"
   default = 2
+  type    = number
 }
 
 variable "app_domain" {
   description = "Domain name for the web application"
   default = ""
+  type    = string
 }
 
 variable "mlflow_domain" {
   description = "Domain name for MLflow"
   default = ""
+  type    = string
 }
 
 variable "email_for_ssl" {
   description = "Email address for SSL certificates"
   default = "admin@example.com"
+  type    = string
 }
 
 variable "unsplash_api_key" {
   description = "Unsplash API key for image search"
   default = ""
   sensitive = true
+  type      = string
 }
 
 variable "use_do_dns" {
   description = "Whether to use DigitalOcean DNS"
   default = false
+  type    = bool
 }
 
 # Outputs
--- terraform/modules/infrastructure/variables.tf
+++ terraform/modules/infrastructure/variables.tf
@@ -1,54 +1,65 @@
 variable "do_token" {
   description = "DigitalOcean API token"
   sensitive = true
+  type        = string
 }
 
 variable "do_region" {
   description = "DigitalOcean region"
   default = "nyc1"
+  type    = string
 }
 
 variable "do_spaces_region" {
   description = "DigitalOcean Spaces region"
   default = "nyc3"
+  type    = string
 }
 
 variable "do_spaces_name" {
   description = "DigitalOcean Spaces bucket name"
   default = "monet-models"
+  type    = string
 }
 
 variable "do_spaces_access_key" {
   description = "DigitalOcean Spaces access key"
   sensitive = true
+  type      = string
 }
 
 variable "do_spaces_secret_key" {
   description = "DigitalOcean Spaces secret key"
   sensitive = true
+  type      = string
 }
 
 variable "cluster_node_size" {
   description = "Size of the Kubernetes nodes"
   default = "s-2vcpu-4gb"
+  type    = string
 }
 
 variable "cluster_node_count" {
   description = "Number of nodes in the Kubernetes cluster"
   default = 2
+  type    = number
 }
 
 variable "app_domain" {
   description = "Domain name for the web application"
   default = ""
+  type    = string
 }
 
 variable "mlflow_domain" {
   description = "Domain name for MLflow"
   default = ""
+  type    = string
 }
 
 variable "use_do_dns" {
   description = "Whether to use DigitalOcean DNS"
   default = false
+  type    = bool
 } 
